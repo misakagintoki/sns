d9021631618dfdd765597efc04efdf335b330f9b
USER <user@USERnoMacBook-Pro.local>
2019-02-22 11:39:09 +0900

    Initial Commit

---
+++ sns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sns
+//
+//  Created by USER on 2019/02/22.
+//  Copyright © 2019 ZZL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
